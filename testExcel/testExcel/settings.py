58a275771373f7fbe43a28791b93ab3e87d556a3
amaniCinetpay <amani.kouakou@cinetpay.com>
2021-08-18 17:52:08 +0000

    DB updated

--- testExcel/testExcel/settings.py
+++ testExcel/testExcel/settings.py
@@ -108,15 +108,15 @@ DATABASES = {
 
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
 
-        'NAME': os.getenv("DB_NAME"),
+        'NAME': os.getenv("DB_NAME", ""),
 
-        'USER': os.getenv("DB_USER"),
+        'USER': os.getenv("DB_USER", ""),
 
-        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'PASSWORD': os.getenv("DB_PASSWORD", ""),
 
-        'HOST': os.getenv("DB_HOST"),
+        'HOST': os.getenv("DB_HOST", ""),
 
-        'PORT': os.getenv("DB_PORT"),
+        'PORT': os.getenv("DB_PORT", ""),
 
     }
 
